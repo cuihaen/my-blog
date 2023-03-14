db73d9d0b65d441bd73722c9e406d6e94fc36252
cuiheen <choi_haeun367@naver.com>
2023-03-14 23:41:19 +0900

    APP초기화

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/detail" element={ <div>상세페이지임</div> } />
-        <Route path="/about" element={ <div>어바웃페이지임</div> } />
-      </Routes>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
     </div>
   );
 }
